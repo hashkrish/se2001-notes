9f1adf9cb86ad86c3b449e228e91586b31bca0ea
Sayan Ghosh <sgic2001@gmail.com>
2024-05-11 12:28:28 +0530

    update ppa1.2 to create script.sh that calls user script for private test cases

--- ppa/ppa_1.2/evaluation_file.sh
+++ ppa/ppa_1.2/evaluation_file.sh
@@ -5,8 +5,8 @@ err(){
   exit 1
 }
 
+executable="filetypes.sh"
 req=( "diff" "basename" "pushd" "popd" "col")
-executable="script.sh"
 for i in "${req[@]}"; do
   command -v "$i" > /dev/null 2>&1 || err "$i is not installed"
 done
@@ -16,48 +16,54 @@ ppa_path="/opt/se2001/$ppa"
 
 [[ -d "$ppa_path" ]] || err "PPA not found at $ppa_path"
 
-for test_type in public private ; do
-  echo "${test_type^} Test Cases:"
-  if [[ ! -d "$ppa_path/$test_type" ]]; then
-    echo "No $test_type test cases found"
+cat >script.sh <<EOF
+#!/bin/bash
+
+rand_dir=\$(mktemp -d XXXXXX)
+pushd "\$rand_dir" > /dev/null || exit 1
+echo "some text" > abc.txt
+xargs touch
+mkdir -p level1
+bash "\$(dirname "\${BASH_SOURCE[0]}")/../$executable" 2>&1 || exit 1
+popd > /dev/null || exit 1
+[[ -d "\$rand_dir" ]] && rm "\${rand_dir?}" -rf
+EOF
+chmod u+x script.sh
+
+test_type="$1"
+test_type=${test_type:-"public"}
+
+if [[ $test_type == "private" ]]; then
+  redir="/dev/null"
+else
+  redir="/dev/stdout"
+fi
+echo "${test_type^} Test Cases:"
+if [[ ! -d "$ppa_path/$test_type" ]]; then
+  err "No $test_type test cases found"
+fi
+tc=0
+passed=0
+IFS=$'\n'
+for test_path in $(find "$ppa_path/$test_type" -type d -name "test_case_*" | sort ); do
+  ((tc++))
+  echo -n "Test Case $tc: "
+  input_path="$test_path/input.txt"
+  output_path="$test_path/output.txt"
+  if [[ ! -f "$output_path" ]]; then
+    echo "Output file for $input_path not found at $output_path"
     continue
   fi
-  tc=0
-  passed=0
-  IFS=$'\n'
-  for input_path in $(find "$ppa_path/$test_type" -type f -name "*.in" ); do
-    ((tc++))
-    echo -n "Test Case $tc: "
-    output_path="${input_path%.in}.out"
-    if [[ ! -f "$output_path" ]]; then
-      echo "Output file for $input_path not found at $output_path"
-      continue
-    fi
-    rand_dir=$(mktemp -d XXXXXX)
-    pushd "$rand_dir" > /dev/null || err "Failed to change directory"
-    echo "some text" > abc.txt
-    while read -r inp; do
-      touch "$inp"
-    done < "$input_path"
-    mkdir -p level1
-    if [[ $test_type == "private" ]]; then
-      redir="/dev/null"
-    else
-      redir="/dev/stdout"
-    fi
-    if diff --color=always <(../script.sh 2>&1 | col) <( col < "$output_path" ) &>"$redir"; then
-      echo "Passed!"
-      ((passed++))
-    else
-      echo "Failed :("
-    fi
-    popd > /dev/null || err "Failed to change directory"
-    rm "${rand_dir?}" -rf
-  done
-  if [[ $passed -eq $tc ]]; then
-    echo "All $test_type test cases passed!"
+  if diff --color=always <(./script.sh < "$input_path" | col) <( col < "$output_path" ) &>"$redir"; then
+    echo "Passed!"
+    ((passed++))
   else
-    echo "$passed/$tc $test_type test cases passed"
-    exit 1
+    echo "Failed :("
   fi
 done
+if [[ $passed -eq $tc ]]; then
+  echo "All $test_type test cases passed!"
+else
+  echo "$passed/$tc $test_type test cases passed"
+  exit 1
+fi
