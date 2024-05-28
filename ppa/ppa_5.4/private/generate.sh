6291c0294f0fd68a48d43bc8e7610a66cc1b5098
Sayan Ghosh <sgic2001@gmail.com>
2024-05-28 23:00:13 +0530

    add ppa_5.4 (refresher week ppa1)

---
+++ ppa/ppa_5.4/private/generate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [[ ! -f ../script.sh ]]; then
+  echo "script.sh not found"
+  exit 1
+fi
+
+if [[ ! -x ../script.sh ]]; then
+  echo "script.sh is not executable"
+  exit 1
+fi
+
+> x00.in
+
+rm test_case_* -rf
+
+for input in *.in; do
+  number=${input%.in}
+  number=${number#x0}
+  test_case="test_case_$number"
+  mkdir -p "$test_case"
+  mv "$input" "$test_case/input.txt" -f
+  ../script.sh < "$test_case/input.txt" > "$test_case/output.txt"
+done
+
+find . -type d -name 'test_case_*' | sort | sed "s/^\./$(basename "$PWD")/" > ../private_test_cases.txt
