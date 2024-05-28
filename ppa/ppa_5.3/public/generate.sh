1a8d861248fe5250b3e4f1bda5d507ed7a418f87
Sayan Ghosh <sgic2001@gmail.com>
2024-05-28 21:27:39 +0530

    add ppa_5.3

---
+++ ppa/ppa_5.3/public/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [[ ! -f ../script.sh ]]; then
+  echo "script.sh not found"
+  exit 1
+fi
+
+if [[ ! -x ../script.sh ]]; then
+  echo "script.sh is not executable"
+  exit 1
+fi
+
+seq 1 18 > x00.in
+seq 1 9 > x01.in
+
+rm test_case_* -rf
+
+for input in *.in; do
+  number=${input%.in}
+  number=${number#x0}
+  test_case="test_case_$number"
+  mkdir -p "$test_case"
+  mv "$input" "$test_case/input.txt" -f
+  ../script.sh < "$test_case/input.txt" > "$test_case/output.txt"
+done
