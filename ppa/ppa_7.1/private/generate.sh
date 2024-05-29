87ba885b6f8bce55a9ee4f928b3d988b67ca3d6e
Sayan Ghosh <sgic2001@gmail.com>
2024-05-29 16:29:59 +0530

    add ppa_7.1

---
+++ ppa/ppa_7.1/private/generate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [[ ! -f ../script.sh ]]; then
+  echo "script.sh not found"
+  exit 1
+fi
+
+if [[ ! -x ../script.sh ]]; then
+  echo "script.sh is not executable"
+  exit 1
+fi
+
+i=0
+for _ in {1..10}; do
+  while [ -z "$num" ] || grep -Fx "$num" x*.in &>/dev/null; do
+    num=$((RANDOM%80+20))
+  done
+  echo "$num" > "x0$i.in"
+  ((i++))
+done
+
+rm test_case_* -rf
+
+for input in *.in; do
+  number=${input%.in}
+  number=${number#x0}
+  test_case="test_case_$number"
+  mkdir -p "$test_case"
+  mv "$input" "$test_case/input.txt" -f
+  ../script.sh < "$test_case/input.txt" > "$test_case/output.txt"
+done
+
+find . -type d -name 'test_case_*' | sort | sed "s/^\./$(basename "$PWD")/" > ../private_test_cases.txt
